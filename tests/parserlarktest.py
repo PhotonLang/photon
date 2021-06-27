aa6bd6d99beb37362b9ab3dbcdead49e2bfbc999
quiktea <wishymovies@gmail.com>
2021-06-27 15:41:35 +0100

    Update tests

--- tests/parserlarktest.py
+++ tests/parserlarktest.py
@@ -1,11 +1,19 @@
-from photon.helpers import read_lark_file
+from photon.helpers import read_lark_file, TimerHelper
 from lark import Lark
 
 grammar = read_lark_file("photon/syntax/grammar.lark")
 
 parser = Lark(grammar)
+timer = TimerHelper()
 
-text = "OPEN 'image.png' AS image"
+text = """
+OPEN 'image.png' AS image
+APPLY 'solarize' TO image
+SAVE image 'output.png'
+"""
+timer.start()
 parser_output = parser.parse(text)
+timer.end()
+print(timer.time() * 1000)
 print(parser_output)
 print(parser_output.pretty())
